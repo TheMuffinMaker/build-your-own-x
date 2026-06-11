1525a65ca03a6d87073348dbbdf6805965f78f70
T.H. Engberg <tore0402@gmail.com>
2026-06-11 15:52:36 +0200

    players can win the game

--- projects/projects-in-c/tic-tac-toe/main.c
+++ projects/projects-in-c/tic-tac-toe/main.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool isFinished(int size, int arr[size][size])
-{
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            if (arr[i][j] == 0)
-                return false;
-    return true;
-}
-
 bool isLegalMove(int size, int arr[size][size], int row, int col)
 {
     if (arr[row][col] != 0)
@@ -18,9 +9,75 @@ bool isLegalMove(int size, int arr[size][size], int row, int col)
         return true;
 }
 
-bool playerHasWon (int size, int arr[size][size], int row, int col, int player)
+bool playerHasWon (int size, int arr[size][size], int player)
 {
+    // check for rows
+    for (int row = 0; row < size; row++)
+    {
+        if(arr[row][0]==arr[row][1]&&
+           arr[row][1]==arr[row][2])
+           {
+               if(arr[row][0]==player)
+               {
+                printf("Player %d won!\n winning sequence was row: %d - %d - %d", player, arr[row][0], arr[row][1], arr[row][2]);
+                return true;
+               }
+           }
+    }
 
+    //check for columns
+    for (int col = 0; col < size; col++)
+    {
+        if(arr[0][col]==arr[1][col]&&
+           arr[1][col]==arr[2][col])
+           {
+               if(arr[0][col]==player)
+               {
+                printf("Player %d won!\n winning sequence was col: %d - %d - %d", player, arr[0][col], arr[1][col], arr[2][col]);
+                return true;
+               }
+           }
+    }
+
+    //check for diagonals
+    if(arr[0][0]==arr[1][1] &&
+     arr[1][1]==arr[2][2])
+    {
+    if(arr[0][0]==player)
+       {
+        printf("Player %d won!\n winning sequence was ltr diagonal: %d - %d - %d", player, arr[0][0], arr[1][1], arr[2][2]);
+        return true;
+       }
+    }
+
+    if(arr[0][2]==arr[1][1] &&
+    arr[1][1]==arr[2][0])
+      {
+        if(arr[0][2]==player)
+           {
+            printf("Player %d won!\n winning sequence was rtl diagonal: %d - %d - %d", player, arr[0][2], arr[1][1], arr[2][0]);
+            return true;
+           }
+      }
+
+      return false;
+}
+
+bool isFinished(int size, int arr[size][size])
+{
+    if (playerHasWon(size, arr, 1))
+    {
+        return true;
+    }
+    else if (playerHasWon(size, arr, 2))
+    {
+        return true;
+    }
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            if (arr[i][j] == 0)
+                return false;
+    return true;
 }
 
 void placeMove (int size, int arr[size][size], int row, int col, int player)
